6322a9239cbd461f88e9049a16c6b5b809c7b806
Alexandr <kitalex777@mail.ru>
2022-12-06 20:14:37 +0300

    my commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        boolean success = Data.validate("test", "test1", "test1");
+        if (success) {
+            System.out.println("Данные валидны");
+        } else {
+            System.out.println("Данные не валидны");
+        }
 
     }
 }
